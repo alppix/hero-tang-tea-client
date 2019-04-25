fe23168c3f348d20e01c885e5b73f19a8ceca023
Derek Fong <derek-fong@users.noreply.github.com>
2019-04-25 17:15:57 +0800

    fix(app): update code coverage output path (#4)
    * Update code coverage output path.
    
    Fix #3

--- src/karma-common.conf.js
+++ src/karma-common.conf.js
@@ -14,7 +14,7 @@ module.exports = {
         clearContext: false // leave Jasmine Spec Runner output visible in browser
       },
       coverageIstanbulReporter: {
-        dir: require('path').join(__dirname, '../coverage/hero-tang-tea'),
+        dir: require('path').join(__dirname, '../coverage'),
         reports: ['html', 'lcovonly', 'text-summary'],
         fixWebpackSourcePaths: true
       },
